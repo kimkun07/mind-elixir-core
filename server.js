0f1b8a088955adc9add6c0069cb95e085f33c55b
kimkun07 <kimkun07@naver.com>
2023-01-31 17:22:14 +0900

    feat: 웹서버로서 작동
    - /server.js 실행하면 됨
    TODO: 다른 서버에 fetch한 json으로 update_map 실행

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const express = require('express')
-const app = express()
-const port = process.env.PORT || 3000
+const server = express()
+const PORT = 3000
 
-app.get('/', (req, res) => {
-  res.json({
-    success: true,
-  })
+const path = require('path')
+const public_path = path.resolve(__dirname, 'public')
+
+server.use(express.static(public_path))
+
+server.get('/', function (req, res) {
+  res.sendFile(path.resolve(public_path, 'index.html'))
 })
 
-app.listen(port, () => {
-  console.log(`server is listening at http://localhost:${port}`)
+server.listen(PORT, function () {
+  console.log(`Server listening - http://localhost:${PORT}`)
 })
